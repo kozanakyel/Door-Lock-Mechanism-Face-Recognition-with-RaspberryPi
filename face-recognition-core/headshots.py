99e90a0db42de7652b45b9d70942a3461488f757
kozanakyel <kozanakyel@pop-os.localdomain>
2022-04-16 21:47:35 +0300

    added finding last image number and path exist for the specific person

--- face-recognition-core/headshots.py
+++ face-recognition-core/headshots.py
@@ -9,12 +9,38 @@ args = parser.parse_args()
 
 name = args.name
 
+#if file or person exist how to find for last images gor continuation the append images
+def find_last_img_number(filelist):
+    lt = []
+    for i in filelist:
+        ptrn = re.findall('[0-9]+', i)
+        lt.append(int(ptrn[0]))
+
+    mx = max(lt)
+    return mx
+
+#preparing for the folder structure before the video capture
+current = os.getcwd()
+path = 'dataset/' + name
+path = os.path.join(current, path)
+
+
+img_counter = 0
+if os.path.exists(path):
+    dir_list = os.listdir(path)
+    print(dir_list, type(dir_list), type(dir_list[0]))
+    print('print last images ', find_last_img_number(dir_list))
+    img_counter = find_last_img_number(dir_list) + 1   #because list index start 0
+else:
+    os.makedirs(path)
+    print(path, 'created...')
+
+
 cam = cv2.VideoCapture(0)
 
 cv2.namedWindow("press space to take a photo", cv2.WINDOW_NORMAL)
 cv2.resizeWindow("press space to take a photo", 500, 300)
 
-img_counter = 0
 
 while True:
     ret, frame = cam.read()
